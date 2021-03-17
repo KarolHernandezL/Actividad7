3817047eb84daedc2c684c2e9b13e7b09bdb3200
KarolHernandezL <khernan2@ibero.edu.co>
2021-03-16 20:28:39 -0500

    ProyectoFinalUnidad2

---
+++ js/jquery.js
@@ -0,0 +1,56 @@
+$(".titulo1").click(function(){
+    $(this).css ("color", "rgb(111, 32, 185)")
+    $(this).css ("font-size", "30px")
+
+})
+
+$(".titulo2").click(function(){
+    $(this).css ("color", "rgb(111, 32, 185)")
+    $(this).css ("font-size", "30px")
+
+})
+
+$(".opcion1").mouseover(function(){
+    $(this).css("font-size","20px")
+})
+
+$(".opcion2").mouseover(function(){
+    $(this).css("font-size","20px")
+})
+
+$(".opcion3").mouseover(function(){
+    $(this).css("font-size","20px")
+})
+
+$(".opcion4").mouseover(function(){
+    $(this).css("font-size","20px")
+})
+
+$(".opcion5").mouseover(function(){
+    $(this).css("font-size","20px")
+})
+
+$(".opcion6").mouseover(function(){
+    $(this).css("font-size","20px")
+})
+
+$(".opcion7").mouseover(function(){
+    $(this).css("font-size","20px")
+})
+
+$(".opcion8").mouseover(function(){
+    $(this).css("font-size","20px")
+})
+
+
+$(".opcion9").mouseover(function(){
+    $(this).css("font-size","20px")
+})
+
+$(".opcion10").mouseover(function(){
+    $(this).css("font-size","20px")
+})
+
+
+
+
